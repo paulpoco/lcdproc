cc928ee0513b4950f2944df9f5f4c707290c4707
paulpoco <paulpoco@gmail.com>
2016-05-04 19:01:51 -0700

    Update lcdproc.sh

--- apps/nobody/lcdproc.sh
+++ apps/nobody/lcdproc.sh
@@ -4,3 +4,19 @@
 if [[ ! -f /config/LCDd.conf ]]; then
 	cp /home/nobody/lcdproc/LCDd.conf /config/
 fi
+
+# if config file doesnt exist then copy stock config file
+if [[ ! -f /config/LCDd.conf ]]; then
+	cp /home/nobody/lcdproc/LCDd.conf /config/
+fi
+
+# if config file doesnt exist then copy stock config file
+if [[ ! -f /config/LCDd.conf ]]; then
+	cp /home/nobody/lcdproc/LCDd.conf /config/
+fi
+
+# if config file doesnt exist then copy stock config file
+if [[ ! -f /config/LCDd.conf ]]; then
+	cp /home/nobody/lcdproc/LCDd.conf /config/
+fi
+
